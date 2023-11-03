55896bceb2daeab301694f2a62f987a9d12c3e0c
Marcelo Florio <mflo80@gmail.com>
2023-11-03 01:06:36 -0300

    Mejoras

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -2,24 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Http;
 
 class InicioController extends Controller
 {
     public function index()
     {
-        $response = Http::withHeaders([ "Accept" => "application/json"])
-        ->get(getenv('GTAPI_TAREAS'));
+        $token = $this->getActiveUserToken(); // Obtiene el token del usuario activo
 
-        $tareas = json_decode($response->body(), true);
-        $tareas = $tareas['tareas'];
+        $response = Http::withHeaders([
+            "Accept" => "application/json",
+            "Authorization" => "Bearer $token" // Incluye el token en los encabezados
+        ])->get(getenv('GTAPI_TAREAS'));
 
-        $tareasPorCategoria = [];
-        foreach ($tareas as $tarea) {
-            $tareasPorCategoria[$tarea['categoria']][] = $tarea;
+        if ($response->successful()) {
+            $tareas = json_decode($response->body(), true);
+            $tareas = $tareas['tareas'] ?? [];
+
+            $tareasPorCategoria = [];
+            foreach ($tareas as $tarea) {
+                $tareasPorCategoria[$tarea['categoria']][] = $tarea;
+            }
+
+            return view('tareas.inicio', ['tareasPorCategoria' => $tareasPorCategoria]);
+        } else {
+            // Maneja el caso en que la respuesta no sea exitosa
+            return redirect()->to('logout')->withErrors([
+                'message' => "Error al obtener las tareas",
+            ]);
         }
+    }
 
-        return view('tareas.inicio', ['tareasPorCategoria' => $tareasPorCategoria]);
+    public function getActiveUserToken()
+    {
+        $userData = Cookie::get('token');
+        $token = json_decode($userData)->token;
+        return $token;
     }
 }
